853160363610dc687bebd429c79289c3bf76a6b4
Zak-2005 <zak.mob2005@gmail.com>
2023-07-10 18:36:43 -0500

    SubHeader Component layout complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
-import React from 'react';
+import React,{useState} from 'react';
+import SubHeader from './components/SubHeader';
 import './App.css';
 
 function App() {
+  const [score,setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0)
   return(
     <div>
     <div className='header'>Programming Memory Game!</div> 
-    <SubHeader />
+    <SubHeader score={score} bestScore={bestScore}/>
     <MainContent />
     </div>
   )
