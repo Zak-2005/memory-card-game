46280c280a70d60af0b105b61d5d88a152b14afd
Zak-2005 <zak.mob2005@gmail.com>
2023-07-10 18:31:06 -0500

    App Component layout complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  
+  return(
+    <div>
+    <div className='header'>Programming Memory Game!</div> 
+    <SubHeader />
+    <MainContent />
+    </div>
+  )
 }
 
 export default App;
