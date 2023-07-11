16efe78dd59280672f832b5f7f42f147b8ca277f
Zak-2005 <zak.mob2005@gmail.com>
2023-07-10 19:13:32 -0500

    Randomize cards on mount

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import React,{useState} from 'react';
-import SubHeader from './components/SubHeader';
-import GenerateContent from './components/GenerateContent'; 
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import SubHeader from "./components/SubHeader";
+import GenerateContent from "./components/GenerateContent";
+import "./App.css";
 
 function App() {
-  const [score,setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0)
-  let src1 = "./Pictures/javascript.png"
-  let src2 = "./Pictures/typescript.png"
-  let src3 = "./Pictures/c.png"
-  let src4 = "./Pictures/c-2.png"
-  let src5 = "./Pictures/java.png"
-  return(
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  let availableImages = [
+    "./Pictures/javascript.png",
+    "./Pictures/typescript.png",
+    "./Pictures/c.png",
+    "./Pictures/c-2.png",
+    "./Pictures/java.png",
+  ];
+
+  const generateSrc = () => {
+    let imgSrc =
+      availableImages[Math.floor(Math.random() * availableImages.length)];
+    availableImages = availableImages.filter((src) => src !== imgSrc);
+    return imgSrc;
+  };
+
+  useEffect(() => {
+    generateSrc();
+  }, []);
+
+  let src1 = generateSrc();
+  let src2 = generateSrc();
+  let src3 = generateSrc();
+  let src4 = generateSrc();
+  let src5 = generateSrc();
+
+  return (
     <div>
-    <div className='header'>Programming Memory Game!</div> 
-    <SubHeader score={score} bestScore={bestScore}/>
-    <GenerateContent cards={[src1,src2,src3,src4,src5]}/>
+      <div className="header">Programming Memory Game!</div>
+      <SubHeader score={score} bestScore={bestScore} />
+      <GenerateContent cards={[src1, src2, src3, src4, src5]} />
     </div>
-  )
+  );
 }
 
 export default App;
